b78678ec9f32c4eda5f3db714b1c3aaf99ccf5e5
Manoj Kumar Barman <infomkbin@gmail.com>
2026-01-16 19:56:14 +0530

    Correct data display and error handling in script.js
    Fix string interpolation and syntax errors in fetch response handling.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ fetch(jsonSource)
 .then(response => response.json())
 .then(data =>{
 console.log(data);
-document.getElementById('data-container').innerHTML='Name:${data.name}, Age: ${data.age}';
+document.getElementById('data-container').innerHTML="Name:"+ ${data.name}"," "Age:"+ ${data.age};;
 })
 .catch(error =>{
 console.error('Error fetching:',error);
+
 });
