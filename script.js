7b3455d6cf77bc431166a8f066b3ab8aeb577201
Manoj Kumar Barman <infomkbin@gmail.com>
2026-01-16 19:48:47 +0530

    new

---
+++ script.js
@@ -0,0 +1,10 @@
+const jsonSource='data.json';
+fetch(jsonSource)
+.then(response => response.json())
+.then(data =>{
+console.log(data);
+document.getElementById('data-container').innerHTML='Name:${data.name}, Age: ${data.age}';
+})
+.catch(error =>{
+console.error('Error fetching:',error);
+});
